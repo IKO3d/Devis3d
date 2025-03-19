a3bbd168aa597ae1d8373d9a01dae53d0f9d2fd2
IKO3d <chareyron.a@live.fr>
2025-03-19 18:15:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 CORS(app)  # ✅ Autorise les requêtes Shopify
+
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 # Prix de base
 BASE_PRICE = 10  # Prix minimum
